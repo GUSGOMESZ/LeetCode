f950800ccb61e08b039cc09ca2f7c8b468c3aa6d
GUSGOMESZ <142702598+GUSGOMESZ@users.noreply.github.com>
2024-06-07 18:39:02 -0300

    1

--- 1_Two_Sun/102ms.c
+++ 1_Two_Sun/102ms.c
@@ -1,4 +1,5 @@
 int* twoSum(int *nums, int numsSize, int target, int *returnSize) {
+  
   for(int i=0; i<numsSize; i++) {
     for(int j=i+1; j<numsSize; j++) {
       if(nums[i] == target - nums[j]) {
