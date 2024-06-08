0372d71f51a352c61031cf68c28d4233589ac26f
GUSGOMESZ <142702598+GUSGOMESZ@users.noreply.github.com>
2024-06-08 18:31:25 -0300

    69

---
+++ 69_Sqrt(x)/11ms.c
@@ -0,0 +1,18 @@
+int mySqrt(int x) {
+  if(x == 0)
+    return 0;
+  else if(x == 1 || x == 2 || x == 3)
+    return 1;
+  else {
+    long i;
+
+    for(i = 2; i < x; i++) {
+      if(i * i >= x) {
+        if(i * i == x)
+          return i;
+        else
+          return (i - 1);
+        }
+      }
+  }
+}
