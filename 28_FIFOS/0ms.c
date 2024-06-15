7600b0abc6d3d4c36f791677d0684eeb9e19daaf
GUSGOMESZ <142702598+GUSGOMESZ@users.noreply.github.com>
2024-06-15 13:18:54 -0300

    28

---
+++ 28_FIFOS/0ms.c
@@ -0,0 +1,31 @@
+int strStr(char *haystack, char * needle) {
+  int i, j, index = -1;
+
+  if(strlen(needle) == 1) {
+    for(i = 0; i < strlen(haystack); i++) {
+      if(haystack[i] == needle[0]) {
+        index = i;
+        break;
+      }
+    }
+  }
+  else {
+    for(i = 0; i < strlen(haystack); i++) {
+      if(haystack[i] == needle[0]) {
+        index = i;
+        for(j = 1; j < strlen(needle); j++) {
+          if(needle[j] == haystack[i + j] {
+            if(j == strlen(needle) - 1) {
+              return index;
+            }
+          }
+          else {
+            index = -1;
+            break;
+          }
+        }
+      }
+    }
+  }  
+  return index;
+}
