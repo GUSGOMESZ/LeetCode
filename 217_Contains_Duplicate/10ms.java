07cc750780a2f2302915cab1423d7bffd9ffbd89
GUSGOMESZ <142702598+GUSGOMESZ@users.noreply.github.com>
2024-06-24 16:24:41 -0300

    .

---
+++ 217_Contains_Duplicate/10ms.java
@@ -0,0 +1,13 @@
+// Beats 88.89%
+
+class Solution {
+  public boolean containsDuplicate(int[] nums) {
+    HashSet<Integer> seen = new HashSet<>();
+    for(int num:nums) {
+      if(seen.contains(num))
+        return true;
+      seen.add(num);
+    }
+    return false;
+  }
+}
