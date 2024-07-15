0f53ca1cba4420462d38847e49fe207796219ed2
GUSGOMESZ <142702598+GUSGOMESZ@users.noreply.github.com>
2024-07-15 13:26:58 -0300

    .

---
+++ 45_Jump_Game_II/0ms.java
@@ -0,0 +1,20 @@
+class Solution {
+  public int jump(int[] nums) {
+    int far = 0, near = 0, jumps = 0;
+
+    while(far < nums.length - 1) {
+      int farthest = 0;
+
+      for(int i = near; i <= far; i++) {
+        if(i + nums[i] > farthest)
+          farthest = i + nums[i];
+      }
+
+      near = far + 1;
+      far = farthest;
+      jumps++;
+    }
+
+    return jumps;
+  }
+}
