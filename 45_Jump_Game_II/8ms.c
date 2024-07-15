3ddc74e9b40aaac2ed535fa1673eb06db63e22b9
GUSGOMESZ <142702598+GUSGOMESZ@users.noreply.github.com>
2024-07-15 13:20:22 -0300

    .

---
+++ 45_Jump_Game_II/8ms.c
@@ -0,0 +1,19 @@
+int jump(int* nums, int numsSize) {
+  int near = 0, far = 0, jumps = 0;
+
+  while(far < numsSize - 1) {
+    int farthest = 0;
+
+    for(int i = near; i <= far; i++) {
+      if(i + nums[i] > farthest) {
+        farthest = i + nums[i];
+      }
+    }
+
+    near = far + 1;
+    far = farthest;
+    jumps++;
+  }
+
+  return jumps;
+}
