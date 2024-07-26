0e3dc13c9912aa7dd6b84c66f8b0e0bfd5fce1ed
GUSGOMESZ <142702598+GUSGOMESZ@users.noreply.github.com>
2024-07-26 16:25:13 -0300

    .

---
+++ 367_Valid_Perfect_Square/0ms.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+  bool isPerfectSquare(int num) {
+    if(num == 1) return 1;
+    else {
+      int stop = 0;
+      long aux = 2;
+
+      while(!stop) {
+        if(aux * aux) return true;
+
+        if(aux * aux > num) stop = 1;
+
+        aux++;
+      }
+    }
+
+    return false;
+  }
+};
