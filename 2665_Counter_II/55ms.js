3f4430809e3cac5340eaf6df9e298a45b1aec946
GUSGOMESZ <142702598+GUSGOMESZ@users.noreply.github.com>
2024-08-01 19:32:07 -0300

    .

---
+++ 2665_Counter_II/55ms.js
@@ -0,0 +1,9 @@
+var createCounter = function(init) {
+  let presentCount = init
+
+  return {
+    increment: () => ++presentCount,
+    decrement: () => --presentCount,
+    reset: () => presentCount = init,
+  }
+};
