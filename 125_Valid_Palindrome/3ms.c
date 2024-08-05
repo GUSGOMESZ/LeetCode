afde5ff15c9e0543bad76e5448592da93ea0edb0
GUSGOMESZ <142702598+GUSGOMESZ@users.noreply.github.com>
2024-08-05 12:51:58 -0300

    .

---
+++ 125_Valid_Palindrome/3ms.c
@@ -0,0 +1,26 @@
+#include <ctype.h>
+
+bool isPalidrome(char* s) {
+  int start = 0, end = strlen(s) - 1;
+
+  while(start <= end) {
+    if(!isalnum(s[start])) {
+      start++;
+      continue;
+    }
+
+    if(!isalnum(s[end])) {
+      end--;
+      continue;
+    }
+
+    if(tolower(s[start]) != tolower(s[end])) {
+      return false;
+    } else {
+      start++;
+      end--;
+    }
+  }
+
+  return true;
+}
