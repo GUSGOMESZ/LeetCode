ef350a6e61f71e6c48d05f6237c487629364dc6a
Gustavo Oliveira Gomes <142702598+GUSGOMESZ@users.noreply.github.com>
2024-08-25 02:02:22 -0300

    .

---
+++ 136_Single_Number/13ms.java
@@ -0,0 +1,21 @@
+class Solution {
+  public int singleNumber(int[] nums) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    for(int i = 0; i < nums.length; i++) {
+      if(!map.containsKey(nums[i])) {
+        map.put(nums[i], 1);
+      } else {
+        map.put(nums[i], (map.get(nums[i]) + 1));
+      }
+    }
+
+    for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+      if(Objects.equals(1, entry.getValue())) {
+        return (entry.getKey());
+      }
+    }
+
+    return 0;
+  }
+}
