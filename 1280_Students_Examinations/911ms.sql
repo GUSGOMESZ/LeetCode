dcf4e9a1018e0b736e222dc5a70afc3e752eb4b3
Gustavo Oliveira Gomes <142702598+GUSGOMESZ@users.noreply.github.com>
2024-10-15 10:47:10 -0300

    .

---
+++ 1280_Students_Examinations/911ms.sql
@@ -0,0 +1,13 @@
+SELECT
+    A.student_id as student_id,
+    A.student_name as student_name,
+    B.subject_name as subject_name,
+    COUNT(C.student_id) as attended_exams 
+FROM
+    Students A CROSS JOIN Subjects B
+        LEFT JOIN Examinations C
+            ON A.student_id = C.student_id AND B.subject_name = C.subject_name
+GROUP BY
+    A.student_id, A.student_name, B.subject_name
+ORDER BY
+    A.student_id, B.subject_name, B.subject_name
