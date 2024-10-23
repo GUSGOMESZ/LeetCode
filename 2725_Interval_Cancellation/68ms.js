b8decb369d06e860ba1d5e47a87c26bbb568242c
Gustavo Oliveira Gomes <142702598+GUSGOMESZ@users.noreply.github.com>
2024-10-23 17:48:38 -0300

    .

---
+++ 2725_Interval_Cancellation/68ms.js
@@ -0,0 +1,10 @@
+var cancellable = function(fn, args, t) {
+    
+    fn(...args);
+
+    let timer = setInterval(() => fn(...args), t);
+
+    let cancelFn = () => clearInterval(timer);
+
+    return cancelFn;
+};
