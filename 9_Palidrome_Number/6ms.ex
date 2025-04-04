316ebd84e0784056ddbcad082d6ff5a80beb52c8
Gustavo Gomes <142702598+GUSGOMESZ@users.noreply.github.com>
2025-04-03 21:44:22 -0300

    .

---
+++ 9_Palidrome_Number/6ms.ex
@@ -0,0 +1,8 @@
+defmodule Solution do
+  @spec is_palindrome(x :: integer) :: boolean
+  def is_palindrome(x) when x < 0, do: false  
+  def is_palindrome(x) do
+    digits = Integer.digits(x)
+    digits == Enum.reverse(digits)
+  end
+end
